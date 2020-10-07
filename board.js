afe555151839a6bd3c64b9616d99b9d6105c4d53
Gabriel <gabriel.gutz@live.com>
2020-10-07 16:48:18 -0400

    Added a ton of Board Functionality

--- board.js
+++ board.js
@@ -1,16 +1,60 @@
+const HumanPlayer = require("./humanPlayer.js");
+
+//gets a random index on the grid --
+function randomIndex(array) {
+    let rowIndex = Math.floor(Math.random() * array.length);
+
+    let columnLength = array[0].length
+
+    let columnIndex = Math.floor(Math.random() * columnLength);
+
+    let row = array[rowIndex]
+
+    row[columnIndex] = 'S'
+
+    return;
+}
+
 class Board {
     constructor(numRows, numCols, numShips) {
         this.numRows = numRows;
         this.numCols = numCols;
+        this.grid = []
         this.populateGrid(numShips);
-        // this.numShips = numShips;
-        // this.grid = [numRows * numCols];
     }
 
     populateGrid(numShips) {
-        const grid = [];
         for (let i = 0; i < this.numRows; i++) {
-            grid.push(Array(this.numCols).fill(null));
+            this.grid.push(Array(this.numCols).fill(null));
         }
+
+        //insert ships --
+        for (let i = 0; i < numShips; i++) {
+            let shipSpot = randomIndex(this.grid);
+        }
+
     }
+
+    shipsLeft(){
+        let counter = 0
+        for (let i = 0; i < this.numRows; i++){
+            let rowIndex = this.grid[i];
+
+            for (let j = 0; j < this.numCols; j++){
+                let columnIndex = rowIndex[j]
+
+                if (columnIndex === "S"){
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+
+
 }
+
+let newBoard = new Board(4, 4, 2);
+
+console.log(newBoard);
